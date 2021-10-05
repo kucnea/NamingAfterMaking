4ae6fcd398637cd545b4c79bc39fa0e656f61e1d
kucnea@naver.com <81126604+kucnea@users.noreply.github.com>
2021-10-06 05:17:02 +0900

    17/ try to insert Player, but i dont know until to try, i must to learn prepersist, sequence .. etc

--- src/main/java/com/web/controller/player/PlayerController.java
+++ src/main/java/com/web/controller/player/PlayerController.java
@@ -1,5 +1,10 @@
 package com.web.controller.player;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -14,6 +19,9 @@ public class PlayerController {
 
 //	@Autowired(required=false)
 //	private PlayerService playerSerivce;
+	
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpaoracle");
+	EntityManager em = emf.createEntityManager();
 	
 	@RequestMapping("login")
 	public String loginPage() {	//url과 method명을 맞추는 것이 관리에 용이
@@ -33,6 +41,27 @@ public class PlayerController {
 		System.out.println("input player to join : "+player.toString());
 		model.addAttribute("player",player);
 		
+		EntityTransaction tx = em.getTransaction();
+		
+		try {
+
+            tx.begin(); //트랜잭션 시작
+            
+            //비즈니스 로직
+            em.persist(player);
+            
+            
+            tx.commit();//트랜잭션 커밋
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback(); //트랜잭션 롤백
+        } finally {
+            em.close(); //엔티티 매니저 종료
+        }
+		emf.close(); //엔티티 매니저 팩토리 종료
+		
+		
 		return "player.joinComplete";
 	}
 
@@ -42,6 +71,4 @@ public class PlayerController {
 		
 		return "player.memberAppointment";
 	}
-	
-	
 }
