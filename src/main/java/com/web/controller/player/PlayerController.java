e4f521129a576c1e88030aea0b7db32aec5b93d8
kucnea@naver.com <81126604+kucnea@users.noreply.github.com>
2021-10-11 05:26:23 +0900

    25/ login controller,session arrange. start making Interceptor

--- src/main/java/com/web/controller/player/PlayerController.java
+++ src/main/java/com/web/controller/player/PlayerController.java
@@ -5,35 +5,33 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
-import com.web.configuration.session.HttpSessionPlayer;
 import com.web.entity.player.Player;
 
 
 @Controller
 @RequestMapping("/player/")
-//@SessionAttributes("player")
+@SessionAttributes("player")
 public class PlayerController {
 
-//	@Autowired(required=false)
-//	private PlayerService playerSerivce;
+	
+	
 	
 	//엔티티 팩토리 싱글톤
 	EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpaoracle");
+
 	
-//	// 세션 싱글톤 -> request가 nullPointException으로 불가
-//	HttpServletRequest request;
-//	HttpSession session = request.getSession();
 	
-	@Autowired(required = false)
-	private HttpSessionPlayer httpSessionPlayer;
+	
+	
 	
 	@RequestMapping("login")
 	public String loginPage() {	//url과 method명을 맞추는 것이 관리에 용이
@@ -41,27 +39,22 @@ public class PlayerController {
 		return "player.login";
 	}
 	
+	
+	
 	@PostMapping("logedin")
 	public String logedIn(String pId, String pPw,Model model, HttpServletRequest request) {
-//	public String logedIn(@ModelAttribute("player") Player player,Model model, HttpServletRequest request) {
 		
-//		HttpSession session = request.getSession();
-//		session.setAttribute("pId", pId);
-		Player player = new Player();
-		player.setpId(pId);
-		player.setpPw(pPw);
-		httpSessionPlayer.setAttribute(player);
+		HttpSession session = request.getSession();
+		session.setAttribute("pId", pId);
 		
 		System.out.println("logedin Player stage // pId : "+pId+", pPw : "+pPw);
-//		model.addAttribute("player",player);
 		
 		
 		EntityManager em = emf.createEntityManager();
 		EntityTransaction tx = em.getTransaction();
+
 		
-//		String pId = player.getpId();
-//		String pPw = player.getpPw();
-//		String pNick = player.getpNick();
+		
 		int flag = 0;	// 1 로그인 성공, 2 실패
 		try {
 
---
+++ src/main/java/com/web/interceptor/FrontInterceptor.java
@@ -0,0 +1,34 @@
+package com.web.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class FrontInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		
+		System.out.println("preHandle");
+		
+		
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			@Nullable ModelAndView modelAndView) throws Exception {
+		
+		System.out.println("afterHandle");
+		
+		
+	}
+	
+	
+	
+}
